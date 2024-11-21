d970bda2b838110df97bc1d5bbfa17352b51fe32
Leonard Peltea <leonard.peltea@yahoo.com>
2024-11-21 16:42:17 +0200

    Adding existing file

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,16 +1,14 @@
-import { GoogleMap, Libraries, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, } from "@react-google-maps/api";
 import { Aside, GoogleMapWrapper, Main } from "./styles";
 
-// aici trebuie ca in MainContainer sa bagi un Aside (adica sidebar) si un GoogleMapWrapper in care sa bagi GoogleMap
-
-const libraries: Libraries = ["places"];
+//const libraries: Libraries = ["places"];
 
 function Map() {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    libraries: libraries,
-    googleMapsApiKey: import.meta.env.VITE_API_KEY,
-  });
+  // const { isLoaded } = useJsApiLoader({
+  //   id: "google-map-script",
+  //   libraries: libraries,
+  //   googleMapsApiKey: import.meta.env.VITE_API_KEY,
+  // });
 
   return (
     <Main>
